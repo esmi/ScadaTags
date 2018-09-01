e2ca02479236bc6e11916158f75933ac5c996fae
esmi <evansmile@gmail.com>
2018-09-01 21:11:32 +0800

    Remove scadaTagJS relative files.

---
+++ example/run.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "scada.php";
+require_once "provider.php";
+
+$provider = new provider();
+$scada = new scada($provider);
+//$scada = new scada(null);
+$d = $scada->scadaTags();
+ ?>
